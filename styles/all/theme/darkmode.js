7a87bf7be2c538c8d4e7a19357976267560ebefb
Daniel Nery <danielnso97@gmail.com>
2020-10-27 11:06:13 -0300

    Change author and color handling

--- styles/all/theme/darkmode.js
+++ styles/all/theme/darkmode.js
@@ -1,49 +1,43 @@
+const colorMap = [
+  ["rgb(239, 83, 80)", "rgb(211, 47, 47)"], // Mecânica
+  ["rgb(236, 64, 122)", "rgb(194, 24, 91)"], // Bixes
+  ["rgb(92, 107, 192)", "rgb(48, 63, 159)"], // Elétrica
+  ["rgb(38, 198, 218)", "rgb(0, 131, 143)"], // Comp
+  ["rgb(171, 71, 188)", "rgb(123, 31, 162)"], // Design
+];
+
+const updateColors = () => {
+  let from = ($("body").hasClass("darkmode") && 1) || 0;
+  let to = (from == 0 && 1) || 0;
+
+  $(".username-coloured, li.recent-topic-item .metadata > a >  span, .online-list .legend a").each(function (i) {
+    colorMap.forEach((e) => {
+      if (this.style.color == e[from]) {
+        this.style.color = e[to];
+      }
+    });
+  });
+};
+
 function darkmode(doDark) {
-	if (doDark) {
-		$("body").addClass("darkmode");
-		$("#callDark").hide();
-		$("#callLight").show();
-		var date = new Date();
-		date.setTime(date.getTime()+(365*24*60*60*1000));
-		document.cookie = "darkmode=true;expires=" + date.toGMTString() + ";path=/";
+  if (doDark) {
+    $("body").addClass("darkmode");
+    $("#callDark").hide();
+    $("#callLight").show();
+    var date = new Date();
+    date.setTime(date.getTime() + 365 * 24 * 60 * 60 * 1000);
+    document.cookie = "darkmode=true;expires=" + date.toGMTString() + ";path=/";
 
-		const colorMap = [
-			["rgb(239, 83, 80)", "rgb(211, 47, 47)"],   // Mecânica
-			["rgb(236, 64, 122)", "rgb(194, 24, 91)"],  // Bixes
-			["rgb(92, 107, 192)", "rgb(48, 63, 159)"],  // Elétrica
-			["rgb(38, 198, 218)", "rgb(0, 131, 143)"],  // Comp
-			["rgb(171, 71, 188)", "rgb(123, 31, 162)"], // Design
-		];
+    updateColors();
+  } else {
+    $("body").removeClass("darkmode");
+    $("#callDark").show();
+    $("#callLight").hide();
+    var date = new Date();
+    document.cookie = "darkmode=false;expires=" + date.toGMTString() + ";path=/";
 
-		$(".username-coloured, li.recent-topic-item .metadata > a >  span, .online-list .legend a").each(function (i) {
-			colorMap.forEach(e => {
-				if (this.style.color == e[1]) {
-					this.style.color = e[0];
-				}
-			});
-		});
-
-	} else {
-		$("body").removeClass("darkmode");
-		$("#callDark").show();
-		$("#callLight").hide();
-		var date = new Date();
-		document.cookie = "darkmode=false;expires=" + date.toGMTString() + ";path=/";
-
-		const colorMap = [
-			["rgb(239, 83, 80)", "rgb(211, 47, 47)"],   // Mecânica
-			["rgb(236, 64, 122)", "rgb(194, 24, 91)"],  // Bixes
-			["rgb(92, 107, 192)", "rgb(48, 63, 159)"],  // Elétrica
-			["rgb(38, 198, 218)", "rgb(0, 131, 143)"],  // Comp
-			["rgb(171, 71, 188)", "rgb(123, 31, 162)"], // Design
-		];
-
-		$(".username-coloured, li.recent-topic-item .metadata > a >  span, .online-list .legend a").each(function (i) {
-			colorMap.forEach(e => {
-				if (this.style.color == e[1]) {
-					this.style.color = e[0];
-				}
-			});
-		});
-	}
+    updateColors();
+  }
 }
+
+updateColors();
