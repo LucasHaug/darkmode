1a7ad565a3d5aefeb71e09236f4e1c0f2456d161
Daniel Nery <danielnso97@gmail.com>
2020-10-27 11:11:45 -0300

    Update js syntax

--- styles/all/theme/darkmode.js
+++ styles/all/theme/darkmode.js
@@ -20,24 +20,24 @@ const updateColors = () => {
 };
 
 function darkmode(doDark) {
+  let date = new Date();
+
   if (doDark) {
     $("body").addClass("darkmode");
     $("#callDark").hide();
     $("#callLight").show();
-    var date = new Date();
+
     date.setTime(date.getTime() + 365 * 24 * 60 * 60 * 1000);
     document.cookie = "darkmode=true;expires=" + date.toGMTString() + ";path=/";
-
-    updateColors();
   } else {
     $("body").removeClass("darkmode");
     $("#callDark").show();
     $("#callLight").hide();
-    var date = new Date();
-    document.cookie = "darkmode=false;expires=" + date.toGMTString() + ";path=/";
 
-    updateColors();
+    document.cookie = "darkmode=false;expires=" + date.toGMTString() + ";path=/";
   }
+
+  updateColors();
 }
 
 updateColors();
