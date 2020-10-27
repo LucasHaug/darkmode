967dccc46ae5233259abf97cb6ff6b561eca0e40
Lucas Haug <lucas.haug@hotmail.com>
2020-10-27 11:28:29 -0300

    :bug: Fix author

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Modified by Lucas Haug.
 
 The colors for darkmode were generate using the `colors.scss` and this [site](https://www.cssportal.com/scss-to-css/) (`sass` can be used as well). `The generate colors were changed a little, so the `darkmode.css` does not correspond to the `colors.scss` entirely. The `colors.scss` file was extracted from a filed called `colors.css.php` that was part of the forum theme.
 
-This extension must be cloned inside the `ext/` and inside of a directory that must be called `aurelienazerty` (the extension original author), so the path to the extension will be `$FORUM_HOME/ext/aurelienazerty/darkmode`.
+This extension must be cloned inside the `ext/` and inside of a directory that must be called `thunderatz`, so the path to the extension will be `$FORUM_HOME/ext/thunderatz/darkmode`.
 
 Known problems:
-- ACP dark mode is a light mode with different colors.
+- ACP has no dark mode.
--- event/listener.php
+++ event/listener.php
@@ -8,7 +8,7 @@
  *
  */
 
-namespace aurelienazerty\darkmode\event;
+namespace thunderatz\darkmode\event;
 
 /**
  * Event listener
@@ -31,7 +31,7 @@ class listener implements EventSubscriberInterface
 	 * @param \phpbb\template\template	$template Template object
 	 * @param \phpbb\user								$user User object
 	 * @param \phpbb\request\request		$request
-	 * @return \aurelienazerty\darkmode\event\listener
+	 * @return \thunderatz\darkmode\event\listener
 	 * @access public
 	 */
 	public function __construct(\phpbb\template\template $template, \phpbb\user $user, \phpbb\request\request $request)
@@ -69,7 +69,7 @@ class listener implements EventSubscriberInterface
 		$this->template->append_var('STYLE_DO_DARK', $styleDoDark);
 		$this->template->append_var('STYLE_DO_LIGHT', $styleDoLight);
 
-		$this->user->add_lang_ext('aurelienazerty/darkmode', 'dark_mode');
+		$this->user->add_lang_ext('thunderatz/darkmode', 'dark_mode');
 		$this->template->assign_var('DO_DARK_MESSAGE'	, $this->user->lang['DO_DARK_MODE']);
 		$this->template->assign_var('DO_LIGHT_MESSAGE'	, $this->user->lang['DO_LIGHT_MODE']);
 	}
